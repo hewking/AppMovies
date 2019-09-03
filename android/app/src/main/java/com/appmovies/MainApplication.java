ab1865f05b1e3ea1cde57caabb1e1a0b4a1857c7
hewking <cjh1095358798@gmail.com>
2019-09-03 20:15:23 +0800

    add  native-measure-text

--- android/app/src/main/java/com/appmovies/MainApplication.java
+++ android/app/src/main/java/com/appmovies/MainApplication.java
@@ -3,6 +3,7 @@ package com.appmovies;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.github.airamrguez.RNMeasureTextPackage;
 import com.rnfs.RNFSPackage;
 import com.shahenlibrary.RNVideoProcessingPackage;
 import com.bolan9999.SpringScrollViewPackage;
@@ -31,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNMeasureTextPackage(),
             new RNFSPackage(),
             new RNVideoProcessingPackage(),
             new SpringScrollViewPackage(),
