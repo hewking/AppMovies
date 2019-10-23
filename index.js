20b97bacf2f949b2eaf026e96578b6c10061e78a
hewking <cjh1095358798@gmail.com>
2019-10-23 16:12:20 +0800

    test code-push success

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ import {name as appName} from './app.json';
 
 import AppContainer from './src/homescreen'
 // import AppContainer from './src/DetailScreen'
-import Main from './src/demo/keyboard/keyboard'
+import Main from './src/homescreen'
 
 AppRegistry.registerComponent(appName, () => Main);
