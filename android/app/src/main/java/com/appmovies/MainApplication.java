974e787d58246318eaa532ac096bd7c6306d1b62
hewking <cjh1095358798@gmail.com>
2019-10-23 14:42:49 +0800

    add code-push config

--- android/app/src/main/java/com/appmovies/MainApplication.java
+++ android/app/src/main/java/com/appmovies/MainApplication.java
@@ -3,6 +3,7 @@ package com.appmovies;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.codepush.react.CodePush;
 import io.github.airamrguez.RNMeasureTextPackage;
 import com.rnfs.RNFSPackage;
 import com.shahenlibrary.RNVideoProcessingPackage;
@@ -24,6 +25,11 @@ public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
+    protected String getJSBundleFile(){
+      return CodePush.getJSBundleFile();
+    }
+
+    @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
     }
@@ -32,6 +38,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG),
             new RNMeasureTextPackage(),
             new RNFSPackage(),
             new RNVideoProcessingPackage(),
